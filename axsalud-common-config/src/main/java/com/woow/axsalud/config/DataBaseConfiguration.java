98f390d115fd501a994b039b8424001a82a38d40
adn201 <adn.dna82@gmail.com>
2025-04-23 20:57:33 +0100

    adding url for DB

--- axsalud-common-config/src/main/java/com/woow/axsalud/config/DataBaseConfiguration.java
+++ axsalud-common-config/src/main/java/com/woow/axsalud/config/DataBaseConfiguration.java
@@ -24,23 +24,31 @@ public class DataBaseConfiguration {
 
     @Bean
     public HikariDataSource getDataSource() throws URISyntaxException {
+        String rawDbUrl = System.getenv("DATABASE_URL");
+        log.info("Using DATABASE_URL: {}", rawDbUrl);
 
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-        log.info("Configuration added in bo-config task");
+        URI dbUri = new URI(rawDbUrl);
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+        String host = dbUri.getHost();
+        int port = dbUri.getPort(); // get the port
+        String dbName = dbUri.getPath().replaceFirst("/", ""); // remove leading slash
 
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url(dbUrl+"?autoReconnect=true");
-        dataSourceBuilder.username(username);
-        dataSourceBuilder.password(password);
-        dataSourceBuilder.type(HikariDataSource.class);
-        HikariDataSource hikariDataSource = (HikariDataSource) dataSourceBuilder.build();
-        hikariDataSource.setMaximumPoolSize(maxPoolSize);
-        hikariDataSource.setMinimumIdle(minimumIdle);
-        return hikariDataSource;
+        // Include port in the JDBC URL
+        String dbUrl = String.format("jdbc:mysql://%s:%d/%s?autoReconnect=true&useSSL=false&serverTimezone=UTC", host, port, dbName);
+
+        HikariDataSource dataSource = DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .build();
+
+        dataSource.setMaximumPoolSize(maxPoolSize);
+        dataSource.setMinimumIdle(minimumIdle);
+
+        return dataSource;
     }
 
 }
