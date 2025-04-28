8988c6217fde23273c779eb4b5f81cd8b5ba48b8
adn201 <adn.dna82@gmail.com>
2025-04-28 18:04:18 +0100

    addding swagger authentication

--- axsalud-common-config/src/main/java/com/woow/axsalud/config/SwaggerConfig.java
+++ axsalud-common-config/src/main/java/com/woow/axsalud/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.woow.axsalud.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +14,24 @@ import org.springframework.context.annotation.Profile;
 @Profile({"dev", "test", "staging"})
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Telemedicine API")
                         .version("1.0")
-                        .description("API documentation for the Telemedicine backend."));
+                        .description("API documentation for the Telemedicine backend."))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 
     @Bean
