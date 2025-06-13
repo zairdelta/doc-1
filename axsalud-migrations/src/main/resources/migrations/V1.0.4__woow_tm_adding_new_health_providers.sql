63baa960c5804c77c16b003564e7f692f30abe3a
adn201 <adn.dna82@gmail.com>
2025-06-13 18:49:21 +0100

    adding AMSAR BIOCRISTAL MI MUTUAL and DEFENSORIA DEL PUEBLO CABA

--- axsalud-migrations/src/main/resources/migrations/V1.0.4__woow_tm_adding_new_health_providers.sql
+++ axsalud-migrations/src/main/resources/migrations/V1.0.4__woow_tm_adding_new_health_providers.sql
@@ -1,4 +1,4 @@
-INSERT INTO full_texts (created_at, endpoint, name, service_expiration)
+INSERT INTO service_provider (created_at, endpoint, name, service_expiration)
 VALUES
   (NOW(), 'https://localhost:8080/external_provider', 'AMSAR', '2025-12-31'),
   (NOW(), 'https://localhost:8080/external_provider', 'BIOCRISTAL', '2025-12-31'),
