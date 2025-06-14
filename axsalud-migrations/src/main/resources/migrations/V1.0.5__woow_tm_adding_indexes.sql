88d882f93fde4c03d2cf33200f4f29639dab0c71
adn201 <adn.dna82@gmail.com>
2025-06-14 12:33:54 +0100

    adding indexes

--- axsalud-migrations/src/main/resources/migrations/V1.0.5__woow_tm_adding_indexes.sql
+++ axsalud-migrations/src/main/resources/migrations/V1.0.5__woow_tm_adding_indexes.sql
@@ -8,4 +8,4 @@ CREATE INDEX idx_consultation_consultationid ON consultation (consultation_id);
 
 CREATE INDEX idx_consultationsession_sessionid ON consultation_session (consultation_session_id);
 
-CREATE INDEX idx_consultationmessage_sessionid ON Consultation_message_entity (consultation_session_id);
+CREATE INDEX idx_consultationmessage_sessionid ON consultation_message_entity (consultation_session_id);
