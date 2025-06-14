b2676c3fefc263d873102ec54e9755e3bef11554
adn201 <adn.dna82@gmail.com>
2025-06-14 12:39:10 +0100

    adding indexes

---
+++ axsalud-migrations/src/main/resources/migrations/U1.0.5__woow_tm_adding_indexes.sql
@@ -0,0 +1,5 @@
+DROP INDEX idx_woowuser_username ON woow_user;
+DROP INDEX idx_woowuser_email ON woow_user;
+DROP INDEX idx_consultation_consultationid ON consultation;
+DROP INDEX idx_consultationsession_sessionid ON consultation_session;
+DROP INDEX idx_consultationmessage_sessionid ON consultation_message_entity;
