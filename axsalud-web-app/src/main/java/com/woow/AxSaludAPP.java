d84748d10036c3f553bd65695e87aae7ee795984
adn201 <adn.dna82@gmail.com>
2025-06-20 09:41:16 +0100

    adding colors in logs

--- axsalud-web-app/src/main/java/com/woow/AxSaludAPP.java
+++ axsalud-web-app/src/main/java/com/woow/AxSaludAPP.java
@@ -3,11 +3,13 @@ package com.woow;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Slf4j
 @EnableScheduling
+@EnableAsync
 public class AxSaludAPP {
 
     public static void main(String[] args) {
